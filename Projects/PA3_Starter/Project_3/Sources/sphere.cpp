e320faacb074f74c199d251526f45385b9e763b8
Brock Handley <brockhandley22@gmail.com>
2024-11-15 15:18:21 -0500

    Project3 Start

---
+++ Projects/PA3_Starter/Project_3/Sources/sphere.cpp
@@ -0,0 +1,81 @@
+#include "sphere.h"
+#include <numbers>
+
+//constructor given  center, radius, and material
+sphere::sphere(glm::vec3 c, float r, int m, scene* s) : rtObject(s)
+{
+	center = c;
+	radius = r;
+	matIndex = m;
+	myScene = s;
+}
+
+float sphere::testIntersection(glm::vec3 eye, glm::vec3 dir)
+{
+	//see the book for a description of how to use the quadratic rule to solve
+	//for the intersection(s) of a line and a sphere, implement it here and
+	//return the minimum positive distance or 9999999 if none
+
+	float discriminant = pow(glm::dot(2.0f * dir, eye - center), 2) + 4.0f * glm::dot(dir, dir) * glm::dot(eye - center, eye - center);
+
+	if (discriminant > 0) {
+		float t1 = (-(glm::dot(dir, eye - center)) + sqrt(pow(glm::dot(dir, eye - center), 2) - (glm::dot(dir, dir) * (glm::dot(eye - center, eye - center) * pow(radius, 2))))) / glm::dot(dir, dir);
+		float t2 = (-(glm::dot(dir, eye - center)) - sqrt(pow(glm::dot(dir, eye - center), 2) - (glm::dot(dir, dir) * (glm::dot(eye - center, eye - center) * pow(radius, 2))))) / glm::dot(dir, dir);
+		if (t1 > 0) {
+			if (t2 > 0 && t2 < t1) {
+				return t2;
+			}
+			else {
+				return t1;
+			}
+		}
+		else {
+			if (t2 > 0) {
+				return t2;
+			}
+		}
+	}
+	return 9999999;
+}
+
+glm::vec3 sphere::getNormal(glm::vec3 eye, glm::vec3 dir)
+{
+	//once you can test for intersection,
+	//simply add (distance * view direction) to the eye location to get surface location,
+	//then subtract the center location of the sphere to get the normal out from the sphere
+	glm::vec3 normal;
+	normal = (testIntersection(eye, dir) * dir) + eye - center;
+
+	//dont forget to normalize
+	normal = glm::normalize(normal);
+	
+	return normal;
+}
+
+glm::vec2 sphere::getTextureCoords(glm::vec3 eye, glm::vec3 dir)
+{
+	//find the normal as in getNormal
+	glm::vec3 normal = getNormal(eye, dir);
+	//use these to find spherical coordinates
+	glm::vec3 x(1, 0, 0);
+	glm::vec3 z(0, 0, 1);
+
+	//phi is the angle down from z
+	//theta is the angle from x curving toward y
+	//hint: angle between two vectors is the acos() of the dot product
+
+	//find phi using normal and z
+	float phi = acos(glm::dot(normal, z));
+	//find the x-y projection of the normal
+	glm::vec3 xyProj(normal.x, normal.y, 0);
+	//find theta using the x-y projection and x
+	float theta = acos(glm::dot(xyProj, x));
+	//if x-y projection is in quadrant 3 or 4, then theta=2*PI-theta
+	if (xyProj.y < 0) {
+		theta = 2 * 3.141 - theta;
+	}
+	//return coordinates scaled to be between 0 and 1
+	glm::vec2 coords(theta, phi);
+	coords = glm::normalize(coords);
+	return coords;
+}
---
+++ Projects/PA3_Starter/Project_3/Sources/triangle.cpp
@@ -0,0 +1,74 @@
+#include "triangle.h"
+
+//constructor given  center, radius, and material
+triangle::triangle(glm::vec3 p0, glm::vec3 p1, glm::vec3 p2, float tx0, float tx1, float tx2, float ty0, float ty1, float ty2, int m, scene* s) : rtObject(s)
+{
+	point0 = p0;
+	point1 = p1;
+	point2 = p2;
+
+	texX0 = tx0;
+	texX1 = tx1;
+	texX2 = tx2;
+	texY0 = ty0;
+	texY1 = ty1;
+	texY2 = ty2;
+	matIndex = m;
+	myScene = s;
+}
+
+float triangle::testIntersection(glm::vec3 eye, glm::vec3 dir)
+{
+	//see the book/slides for a description of how to use Cramer's rule to solve
+	//for the intersection(s) of a line and a plane, implement it here and
+	//return the minimum distance (if barycentric coordinates indicate it hit
+	//the triangle) otherwise 9999999
+
+	float a = point0.x - point1.x;
+	float b = point0.y - point1.y;
+	float c = point0.z - point1.z;
+	float d = point0.x - point2.x;
+	float e = point0.y - point2.y;
+	float f = point0.z - point2.z;
+	float g = dir.x;
+	float h = dir.y;
+	float i = dir.z;
+	float j = point0.x - eye.x;
+	float k = point0.y - eye.y;
+	float l = point0.z - eye.z;
+
+	float M = a * (e * i - h * f) + b * (g * f - d * i) + c * (d * h + e * g);
+
+	float beta = (j * (e * i - h * f) + k * (g * f - d * i) + l * (d * h - e * g)) / M;
+	float gamma = (i * (a * k - j * b) + h * (j * c - a * l) + g * (b * l - k * c)) / M;
+	float t = -1 * ((f * (a * k - j * b) + e * (j * c - a * l) + d * (b * l * k * c)) / M);
+
+	if (beta > 0 && gamma > 0 && beta + gamma < 1) {
+		return t;
+	}
+
+	return 9999999;
+}
+
+glm::vec3 triangle::getNormal(glm::vec3 eye, glm::vec3 dir)
+{
+	//construct the barycentric coordinates for the plane
+	glm::vec3 bary1 = point1 - point0;
+	glm::vec3 bary2 = point2 - point0;
+
+	//cross them to get the normal to the plane
+	//note that the normal points in the direction given by right-hand rule
+	//(this can be important for refraction to know whether you are entering or leaving a material)
+	glm::vec3 normal = glm::normalize(glm::cross(bary1, bary2));
+	return normal;
+}
+
+glm::vec2 triangle::getTextureCoords(glm::vec3 eye, glm::vec3 dir)
+{
+	//find alpha and beta (parametric distance along barycentric coordinates)
+	//use these in combination with the known texture surface location of the vertices
+	//to find the texture surface location of the point you are seeing
+
+	glm::vec3 coords;
+	return coords;
+}
